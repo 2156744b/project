052c3f4dde7adb64c02ac5f20c5c45890e6dd6be
2156744b <2156744b@student.gla.ac.uk>
2015-07-24 18:46:01 +0000

    Added execution engine to scripts

--- tpch/queries/hive_tez/run_queries.sh
+++ tpch/queries/hive_tez/run_queries.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 set -e
 
-if [ -z "$1" ]; then
-	echo "No target foldername supplied"
-	echo "Run $0 foldername"
-	exit		
-fi
-
 source ../../../executables.conf
-
-RESULTS="../../results/hive_tez"
-mkdir -p $RESULTS/$1 
-
-$HIVE -e "set hive.execution.engine=tez;"
 
 for query in *.hive; do
 	echo -e "\n\n************************************************"
 	echo "Executing" $query
 	echo -e "************************************************\n\n"
 	filename=`echo $query | cut -d "_" -f 1`
-	$HIVE -f $query 2>&1 | tee $RESULTS/$1/${filename}_result.txt 
+	$HIVE -f $query #2>&1 | tee $RESULTS/$1/${filename}_result.txt 
 done
