2667a92682f1a6394b4c25d8fe45e895f195e687
2156744b <2156744b@student.gla.ac.uk>
2015-08-05 20:31:26 +0100

    Changed running script

--- tpch/sf_1/queries/q11/run_queries.sh
+++ tpch/sf_1/queries/q11/run_queries.sh
@@ -5,23 +5,23 @@ source ~/project/executables.conf
 RESULTS=results
 mkdir -p $RESULTS
 
-DATE=`date | sed 's/ //g'`
+suffix=$1
 
 for f in *.hive; do
-	result=$RESULTS/${f}_$DATE
+	result=$RESULTS/${f}_$suffix
 	cat $f > $result
 	$HIVE -f $f 2>&1 | tee -a $result
 done
 
 for f in *.pig; do
-	result=$RESULTS/${f}_$DATE
+	result=$RESULTS/${f}_$suffix
         cat $f > $result
         $PIG -x tez -f $f 2>&1 | tee -a $result
 
 done
 
 for f in *.spark; do
-	result=$RESULTS/${f}_$DATE
+	result=$RESULTS/${f}_$suffix
         cat $f > $result
-	sudo -u hive $SPARK --driver-memory 6g --executor-memory 512m --num-executors 4 -f $f | tee -a $result
+	sudo -u hive $SPARK --driver-memory 6g --executor-memory 512m --num-executors 4 -f $f 2>&1 | tee -a $result
 done
