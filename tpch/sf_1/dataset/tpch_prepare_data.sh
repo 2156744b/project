cca410b59c0633f7a66d879f3fbcc65038995f40
2156744b <2156744b@student.gla.ac.uk>
2015-08-10 16:02:52 +0100

    Updated for new SPARK version 1.4

--- tpch/sf_1/dataset/tpch_prepare_data.sh
+++ tpch/sf_1/dataset/tpch_prepare_data.sh
@@ -1,30 +1,31 @@
 source ~/project/executables.conf
 
 $HADOOP fs -mkdir tpch/ && echo "mkdir tpch/"
-$HADOOP fs -mkdir tpch/customer_sf_1 && echo "mkdir customer_sf_1"
-$HADOOP fs -mkdir tpch/lineitem_sf_1 && echo "mkdir lineitem_sf_1"
-$HADOOP fs -mkdir tpch/nation_sf_1 && echo "mkdir nation_sf_1"
-$HADOOP fs -mkdir tpch/orders_sf_1 && echo "mkdir orders_sf_1"
-$HADOOP fs -mkdir tpch/part_sf_1 && echo "mkdir part_sf_1"
-$HADOOP fs -mkdir tpch/partsupp_sf_1 && echo "mkdir partsupp_sf_1"
-$HADOOP fs -mkdir tpch/region_sf_1 && echo "mkdir region_sf_1"
-$HADOOP fs -mkdir tpch/supplier_sf_1 && echo "mkdir supplier_sf_1"
+$HADOOP fs -mkdir tpch/sf_1 && echo "mkdir tpch/sf_1"
+$HADOOP fs -mkdir tpch/sf_1/customer && echo "mkdir customer"
+$HADOOP fs -mkdir tpch/sf_1/lineitem && echo "mkdir lineitem"
+$HADOOP fs -mkdir tpch/sf_1/nation && echo "mkdir nation"
+$HADOOP fs -mkdir tpch/sf_1/orders && echo "mkdir orders"
+$HADOOP fs -mkdir tpch/sf_1/part && echo "mkdir part"
+$HADOOP fs -mkdir tpch/sf_1/partsupp && echo "mkdir partsupp"
+$HADOOP fs -mkdir tpch/sf_1/region && echo "mkdir region"
+$HADOOP fs -mkdir tpch/sf_1/supplier && echo "mkdir supplier"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/customer.tbl tpch/customer_sf_1/ && echo "customer loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/customer.tbl tpch/sf_1/customer/ && echo "customer loaded"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/lineitem.tbl tpch/lineitem_sf_1/ && echo "lineitem loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/lineitem.tbl tpch/sf_1/lineitem/ && echo "lineitem loaded"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/nation.tbl tpch/nation_sf_1/ && echo "nation loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/nation.tbl tpch/sf_1/nation/ && echo "nation loaded"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/orders.tbl tpch/orders_sf_1/ && echo "orders loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/orders.tbl tpch/sf_1/orders/ && echo "orders loaded"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/part.tbl tpch/part_sf_1/ && echo "part loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/part.tbl tpch/sf_1/part/ && echo "part loaded"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/partsupp.tbl tpch/partsupp_sf_1/ && echo "partsupp loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/partsupp.tbl tpch/sf_1/partsupp/ && echo "partsupp loaded"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/region.tbl tpch/region_sf_1/ && echo "region loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/region.tbl tpch/sf_1/region/ && echo "region loaded"
 
-$HADOOP fs -copyFromLocal ~/project/tpch/dataset/supplier.tbl tpch/supplier_sf_1/ && echo "supplier loaded"
+$HADOOP fs -copyFromLocal ~/project/tpch/dataset/supplier.tbl tpch/sf_1/supplier/ && echo "supplier loaded"
 
 $HIVE -f tpch_external_tables.hive
 $HIVE -f tpch_dataset.hive
