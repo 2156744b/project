1aa89a764fe7915a8368bf0520006e57c86067ca
2156744b <2156744b@student.gla.ac.uk>
2015-08-30 23:15:41 +0000

    Added query plans for q11 versions

---
+++ tpch/sf_100/queries/q11/explain/q11_parquet_v1.sql
@@ -0,0 +1,27 @@
+-- the query
+explain
+select
+  ps_partkey,
+  sum(ps_supplycost * ps_availqty) as part_value
+from
+  nation_par n join supplier_par s on s.s_nationkey = n.n_nationkey and n.n_name = 'RUSSIA'
+  join partsupp_par ps on ps.ps_suppkey = s.s_suppkey
+group by
+    ps_partkey;
+
+explain
+select
+  sum(part_value) as total_value
+from
+  q11_part_tmp_par;
+
+explain
+select
+  ps_partkey, part_value as value
+from
+  (
+    select ps_partkey, part_value, total_value
+    from q11_part_tmp_par join q11_sum_tmp_par
+  ) a
+where part_value > total_value * 0.000001
+order by value desc;
